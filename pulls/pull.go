d56abb7fd43f60b0e86fd978bf8c5efd85188697
zkfy <lib4net@163.com>
2020-11-20 09:32:32 +0800

    添加main分支拉取

--- pulls/pull.go
+++ pulls/pull.go
@@ -2,6 +2,7 @@ package pulls
 
 import (
 	"fmt"
+
 	"github.com/micro-plat/cli/cmds"
 	"github.com/micro-plat/cli/logs"
 	"github.com/micro-plat/gitcli/gitlabs"
@@ -24,25 +25,29 @@ func init() {
 		})
 }
 
+var pullBranch = []string{"master", "main"}
+
 //pull 根据传入的路径(分组/仓库)拉取所有项目
 func pull(c *cli.Context) (err error) {
-	branch := types.GetString(c.String("branch"), "master")
-	reps, err := gitlabs.GetRepositories(c.Args().Get(0))
-	if err != nil {
-		return err
-	}
-	if len(reps) == 0 {
-		return fmt.Errorf("没有需要拉取的项目")
-	}
-	for _, rep := range reps {
-		if !rep.Exists() {
-			logs.Log.Infof("get clone %s %s", rep.FullPath, rep.GetLocalPath())
-			if err := rep.Clone(); err != nil {
+	for _, b := range pullBranch {
+		branch := types.GetString(c.String("branch"), b)
+		reps, err := gitlabs.GetRepositories(c.Args().Get(0))
+		if err != nil {
+			return err
+		}
+		if len(reps) == 0 {
+			return fmt.Errorf("没有需要拉取的项目")
+		}
+		for _, rep := range reps {
+			if !rep.Exists() {
+				logs.Log.Infof("get clone %s %s", rep.FullPath, rep.GetLocalPath())
+				if err := rep.Clone(); err != nil {
+					logs.Log.Error(err)
+				}
+			}
+			if err := rep.Pull(branch); err != nil {
 				logs.Log.Error(err)
 			}
-		}
-		if err := rep.Pull(branch); err != nil {
-			logs.Log.Error(err)
 		}
 	}
 	return nil
