46e4b6dda1d7bd692cebd3894c710bee5327c11d
zkfy <lib4net@163.com>
2021-01-16 10:43:46 +0800

    添加enums生成代码

---
+++ markdown/app/tmpl.enums.go
@@ -0,0 +1,47 @@
+package app
+
+//TmplEnumsHandler 服务处理函数
+const TmplEnumsHandler = `
+package {{.PKG}}
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/micro-plat/hydra"
+)
+
+//EnumsHandler 枚举数据查询服务
+type EnumsHandler struct {
+}
+
+//QueryHandle 枚举数据查询服务
+func (o *EnumsHandler) QueryHandle(ctx hydra.IContext) interface{} {
+
+	//根据传入的枚举类型获取数据
+	tp := ctx.Request().GetString("type")
+	if tp != "" {
+		items, err := hydra.C.DB().GetRegularDB().Query(enumsMap[tp], ctx.Request().GetMap())
+		if err != nil {
+			return err
+		}
+	}
+
+	//查询所有枚举数据
+	list := types.XMaps{}
+	for _, sql := range enumsMap {
+		items, err := hydra.C.DB().GetRegularDB().Query(enumsMap[tp], ctx.Request().GetMap())
+		if err != nil {
+			return err
+		}
+		list = append(list, items...)
+	}
+	return list
+
+}
+
+var enumsMap = map[string]string{
+{{range $j,$t:=.Tbs}}
+{{- if $t|isEnumTB}}
+"{{$t.Name|rmhd}}":{###}select '{{$t.Name|rmhd}}' 'type',{{range $i,$c:=.Rows -}}{{if $c.Con|isDI}}t.{{$c.Name}} 'value',{{end}}{{if $c.Con|isDN}}t.{{$c.Name}} 'name' {{end}}{{end -}}from {{$t.Name}} t {###},
+{{- end}}
+{{end}}
+}`
