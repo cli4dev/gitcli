65b8952236fcda04a88323e83304982f720de47c
hj <546921722@qq.com>
2021-03-08 19:11:29 +0800

    修改前端代码生成

--- markdown/ui/tmpl.list.vue.go
+++ markdown/ui/tmpl.list.vue.go
@@ -259,10 +259,10 @@ export default {
     del(val){
 			this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {confirmButtonText: "确定",  cancelButtonText: "取消", type: "warning"})
 			.then(() => {
-				this.$http.del("/{{.Name|rmhd|rpath}}", {data:val}, {}, true, true)
+				this.$http.del("/{{.Name|rmhd|rpath}}",val, {}, true, true)
 				.then(res => {			
 					this.dialogFormVisible = false;
-					this.refresh()
+					this.query()
 				})
       }).catch(() => {
         this.$message({
